fc8d8a5ceb41b4f7a43c5607e79bbd8154481201
Chris <chriscablish@hotmail.com>
2023-03-01 07:39:25 -0600

    program show/hide functionality with js

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const button = document.querySelector('button')
+
+const shareBtn = document.querySelector(".share-btn");
+const hiddenShareBtn = document.querySelector(".hidden-share-btn");
+const shareSection = document.querySelector(".share-section");
+const shareLink = document.querySelector(".share-link");
+const socialMediaSection = document.querySelector(".social-icons");
+
+const toggleSection = () => shareSection.classList.toggle("shown");
+
+shareBtn.addEventListener("click", function () {
+  toggleSection();
+});
+hiddenShareBtn.addEventListener("click", function () {
+  toggleSection();
+});
+
+
+
 
 
 
